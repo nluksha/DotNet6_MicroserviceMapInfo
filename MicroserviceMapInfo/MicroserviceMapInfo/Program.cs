56dfca1b1992299d89e48efec957987f72d92904
Nikita Luksha <nikita.luksha@softensity.com>
2022-02-03 20:41:43 +0300

    Refactor

--- MicroserviceMapInfo/MicroserviceMapInfo/Program.cs
+++ MicroserviceMapInfo/MicroserviceMapInfo/Program.cs
@@ -1,12 +1,18 @@
+using Microsoft.OpenApi.Models;
+using GoogleMapInfo;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddTransient<GoogleMapInfo.GoogleDistanceApi>();
+builder.Services.AddTransient<GoogleDistanceApi>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen( c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My map API", Version = "v1" });
+});
 
 var app = builder.Build();
 
