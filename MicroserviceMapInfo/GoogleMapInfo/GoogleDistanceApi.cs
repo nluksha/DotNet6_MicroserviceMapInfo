a1c2fefefe8f13cc27965e08238e27cfe2c98440
Nikita Luksha <nikita.luksha@softensity.com>
2022-02-03 20:36:48 +0300

    Add MapInfo controller

--- MicroserviceMapInfo/GoogleMapInfo/GoogleDistanceApi.cs
+++ MicroserviceMapInfo/GoogleMapInfo/GoogleDistanceApi.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace GoogleMapInfo
@@ -18,7 +19,21 @@ namespace GoogleMapInfo
 
         public async Task<GoogleDistanceData> GetMapDistanceAsync(string originCity, string destinationCity)
         {
-            // todo
+            var apiKey = configuration["googleDistanceApi:apiKey"];
+            var apiUrl = configuration["googleDistanceApi:apiUrl"];
+
+            apiUrl += $"unit=imperial&origins={originCity}&destinations={destinationCity}&key={apiKey}";
+
+            using var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, new Uri(apiUrl));
+
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            await using var data = await response.Content.ReadAsStreamAsync();
+            var distanceInfo = await JsonSerializer.DeserializeAsync<GoogleDistanceData>(data);
+
+            return distanceInfo;
         }
     }
 }
---
+++ MicroserviceMapInfo/MicroserviceMapInfo/Controllers/MapInfoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GoogleMapInfo;
+
+namespace MicroserviceMapInfo.Controllers
+{
+    [Route("[controller]")]
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class MapInfoController : ControllerBase
+    {
+        private readonly GoogleDistanceApi googleDistanceApi;
+
+        public MapInfoController(GoogleDistanceApi googleDistanceApi)
+        {
+            this.googleDistanceApi = googleDistanceApi;
+        }
+
+        [HttpGet]
+        public async Task<GoogleDistanceData> GetDistance(string originCity, string destinationCity)
+        {
+            return await googleDistanceApi.GetMapDistanceAsync(originCity, destinationCity);
+        }
+    }
+}
