bd78c9e2ab9285186e7782ad925ba5bcf380a349
Nikita Luksha <nikita.luksha@softensity.com>
2022-02-03 10:06:21 +0300

    Add google data classes

---
+++ MicroserviceMapInfo/GoogleMapInfo/GoogleDistanceApi.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleMapInfo
+{
+    public class GoogleDistanceApi
+    {
+        private readonly IConfiguration configuration;
+
+        public GoogleDistanceApi(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public async Task<GoogleDistanceData> GetMapDistanceAsync(string originCity, string destinationCity)
+        {
+            // todo
+        }
+    }
+}
